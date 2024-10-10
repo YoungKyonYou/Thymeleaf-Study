0414ce1961fb921a083d4b4cbfd86051903077f2
YoungKyonYou <man.spride1324@gmail.com>
2024-10-10 17:51:47 +0900

    feat: register feature

--- guestbook/src/main/java/com/youyk/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/youyk/guestbook/controller/GuestbookController.java
@@ -10,8 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -29,6 +31,22 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list............."+pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    // localhost:8080/guestbook/register로 접속하기 위한 메소드
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    //submit 버튼을 눌렀을 때 처리하는 메소드
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto.."+dto);
+        Long gno = service.register(dto);
+        //한 번만 화면에서 "msg"라는 이름의 변수를 사용할 수 있도록 처리함
+        redirectAttributes.addFlashAttribute("msg",gno);
+        return "redirect:/guestbook/list";
     }
 
 }
